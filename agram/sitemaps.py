dbd5412d068f51c6c8fe86694b429dbe58a659c1
Ahmed <ofkif90@gmail.com>
2026-07-20 22:19:34 +0300

    Fix StaticViewSitemap definition

--- agram/sitemaps.py
+++ agram/sitemaps.py
@@ -1,21 +1,12 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 
-
 class StaticViewSitemap(Sitemap):
+    priority = 0.5
     changefreq = "daily"
-    priority = 0.8
 
     def items(self):
-        return [
-            "index",
-            "guide",
-            "about",
-            "contact",
-            "privacy",
-            "terms",
-            "faq",
-        ]
+        return ["index", "guide", "about", "contact", "privacy", "terms", "faq"]
 
     def location(self, item):
         return reverse(item)
